fe02b9a6db9f1cda17a3f10367033abcfbff524f
Chris Jordan <chris@vtail.co>
2022-11-16 10:24:59 +0000

    working chord progressions with octave

--- components/helpers/getChordProgression.test.ts
+++ components/helpers/getChordProgression.test.ts
@@ -1,10 +1,20 @@
-import { getChordProgression } from "./getChordProgression";
+import { getChordProgression, getScale } from "./getChordProgression";
 
-test("1564 C", () => {
+test("getScale", () => {
+  expect(getScale("C")).toEqual(["C", "D", "E", "F", "G", "A", "B"]);
+  expect(getScale("D")).toEqual(["D", "E", "F#", "G", "A", "B", "C#"]);
+  expect(getScale("E")).toEqual(["E", "F#", "G#", "A", "B", "C#", "D#"]);
+  expect(getScale("F")).toEqual(["F", "G", "A", "A#", "C", "D", "E"]);
+  expect(getScale("G")).toEqual(["G", "A", "B", "C", "D", "E", "F#"]);
+  expect(getScale("A")).toEqual(["A", "B", "C#", "D", "E", "F#", "G#"]);
+  expect(getScale("B")).toEqual(["B", "C#", "D#", "E", "F#", "G#", "A#"]);
+});
+
+test("I V vi IV C", () => {
   expect(getChordProgression("C", ["I", "V", "vi", "IV"], 3)).toEqual([
-    ["C3", "E3", "G3"],
-    ["G3", "B3", "D3"],
-    ["A3", "C4", "E4"],
-    ["F3", "A3", "C4"],
+    { label: "I", progression: ["C3", "E3", "G3"] },
+    { label: "V", progression: ["G3", "B3", "D4"] },
+    { label: "vi", progression: ["A3", "C4", "E4"] },
+    { label: "IV", progression: ["F3", "A3", "C4"] },
   ]);
 });
--- components/helpers/getChordProgression.ts
+++ components/helpers/getChordProgression.ts
@@ -6,56 +6,15 @@ const notes = [
   "D",
   "D#",
   "E",
-  "Eb",
   "F",
   "F#",
   "G",
   "G#",
   "A",
   "A#",
-  "Ab",
   "B",
-  "Bb",
 ] as const;
 type Note = typeof notes[number];
-
-type Chord = [Note, Note, Note];
-
-const majorChords: Record<Note, Chord> = {
-  Ab: ["Ab", "C", "Eb"],
-  Bb: ["Bb", "D", "F"],
-  Eb: ["Eb", "G", "Bb"],
-  "A#": ["A#", "C#", "E"],
-  "C#": ["C#", "E", "G#"],
-  "D#": ["D#", "F#", "A#"],
-  "F#": ["F#", "A#", "C#"],
-  "G#": ["G#", "B", "D#"],
-  C: ["C", "E", "G"],
-  D: ["D", "F#", "A"],
-  E: ["E", "G#", "B"],
-  F: ["F", "A", "C"],
-  G: ["G", "B", "D"],
-  A: ["A", "C#", "E"],
-  B: ["B", "D#", "F#"],
-};
-
-const minorChords: Record<Note, Chord> = {
-  "A#": ["A#", "C#", "E"],
-  "C#": ["C#", "E", "G#"],
-  "D#": ["D#", "F#", "A#"],
-  "F#": ["F#", "A#", "C#"],
-  "G#": ["G#", "B", "D#"],
-  Ab: ["Ab", "C", "Eb"],
-  Bb: ["Bb", "D", "F"],
-  Eb: ["Eb", "G", "Bb"],
-  C: ["C", "Eb", "G"],
-  D: ["D", "F", "A"],
-  E: ["E", "G", "B"],
-  F: ["F", "Ab", "C"],
-  G: ["G", "Bb", "D"],
-  A: ["A", "C", "E"],
-  B: ["B", "D", "F#"],
-};
 
 type ChordWithOctave = [
   `${Note}${number}`,
@@ -65,23 +24,50 @@ type ChordWithOctave = [
 
 type ChordProgression = { label: string; progression: ChordWithOctave }[];
 
-const getChord2 = (
-  key: Key,
-  chordType: "major" | "minor",
-  note: number,
+export function getScale(key: Key): Note[] {
+  const startIndex = notes.findIndex((note) => note === key);
+
+  const first = notes.at(startIndex % notes.length);
+  const second = notes.at((startIndex + 2) % notes.length);
+  const third = notes.at((startIndex + 4) % notes.length);
+  const fourth = notes.at((startIndex + 5) % notes.length);
+  const fifth = notes.at((startIndex + 7) % notes.length);
+  const sixth = notes.at((startIndex + 9) % notes.length);
+  const seventh = notes.at((startIndex + 11) % notes.length);
+
+  return [first!, second!, third!, fourth!, fifth!, sixth!, seventh!];
+}
+
+const getChord3 = (
+  scale: Note[],
+  scaleType: "major" | "minor",
+  chordRoot: number,
   octave: number
 ): ChordWithOctave => {
-  const keyIndex = keys.indexOf(key);
-  const chordKey = keys[(keyIndex + note) % 7];
+  const root = scale[chordRoot];
+  const keyIndex = notes.findIndex((note) => note === root);
+  if (scaleType === "major") {
+    const third = notes[(keyIndex + 4) % 12];
+    const thirdOctave = keyIndex + 4 > 11 ? octave + 1 : octave;
+    const fifth = notes[(keyIndex + 7) % 12];
+    const fifthOctave = keyIndex + 7 > 11 ? octave + 1 : octave;
 
-  if (chordType === "major") {
-    return majorChords[chordKey].map(
-      (note) => `${note}${octave}`
-    ) as ChordWithOctave;
+    return [
+      `${root}${octave}`,
+      `${third}${thirdOctave}`,
+      `${fifth}${fifthOctave}`,
+    ];
   } else {
-    return minorChords[chordKey].map(
-      (note) => `${note}${octave}`
-    ) as ChordWithOctave;
+    const third = notes[(keyIndex + 3) % 12];
+    const thirdOctave = keyIndex + 3 > 11 ? octave + 1 : octave;
+    const fifth = notes[(keyIndex + 7) % 12];
+    const fifthOctave = keyIndex + 7 > 11 ? octave + 1 : octave;
+
+    return [
+      `${root}${octave}`,
+      `${third}${thirdOctave}`,
+      `${fifth}${fifthOctave}`,
+    ];
   }
 };
 
@@ -90,24 +76,25 @@ export const getChordProgression = (
   progression: string[],
   octave: number
 ): ChordProgression => {
+  const scale = getScale(key);
   const results: [string, ChordWithOctave][] = progression.map((chord) => {
     switch (chord) {
       case "I":
-        return ["I", getChord2(key, "major", 0, octave)];
+        return ["I", getChord3(scale, "major", 0, octave)];
       case "ii":
-        return ["ii", getChord2(key, "minor", 1, octave)];
+        return ["ii", getChord3(scale, "minor", 1, octave)];
       case "iii":
-        return ["iii", getChord2(key, "minor", 2, octave)];
+        return ["iii", getChord3(scale, "minor", 2, octave)];
       case "IV":
-        return ["IV", getChord2(key, "major", 3, octave)];
+        return ["IV", getChord3(scale, "major", 3, octave)];
       case "V":
-        return ["V", getChord2(key, "major", 4, octave)];
+        return ["V", getChord3(scale, "major", 4, octave)];
       case "vi":
-        return ["vi", getChord2(key, "minor", 5, octave)];
+        return ["vi", getChord3(scale, "minor", 5, octave)];
       case "vii":
-        return ["vii", getChord2(key, "minor", 6, octave)];
+        return ["vii", getChord3(scale, "minor", 6, octave)];
       default:
-        return ["I", getChord2(key, "major", 0, octave)];
+        return ["I", getChord3(scale, "major", 0, octave)];
     }
   });
 
