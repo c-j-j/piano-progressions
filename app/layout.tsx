1ea2f04c7fbe17dbd522c9bc5f9c8c017b5dd0d2
Chris Jordan <chris@vtail.co>
2022-11-21 12:56:54 +0000

    basic nav

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import "./globals.css";
 
 export default function RootLayout({
@@ -12,7 +13,17 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body className="bg-slate-100">{children}</body>
+      <body className="bg-slate-100">
+        <nav className="bg-slate-800 text-white p-4 sticky top-0 mb-8">
+          <div className="max-w-6xl m-auto flex justify-between">
+            <Link href="/">Home</Link>
+            <div className="invisible md:visible">
+              <Link href="/">Account</Link>
+            </div>
+          </div>
+        </nav>
+        {children}
+      </body>
     </html>
   );
 }
